921ab25324b228df6e1cd9e7131a82f2c7943bf1
Quynh <bequynh09@gmail.com>
2020-08-26 15:36:57 -0400

    Die.js stateful to stateless component.

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,31 +6,10 @@ import { faDiceSix, faDiceFive, faDiceFour, faDiceThree, faDiceTwo, faDiceOne, f
 
 library.add(faDiceSix, faDiceFive, faDiceFour, faDiceThree, faDiceTwo, faDiceOne, faDiceD6);
 
-class Die extends Component {
-
-    constructor(props) {
-        
-        super(props);
-        
-        this.state = {
-            dieFace: this.props.dieFace,
-            dieValue: this.props.dieValue,
-            dieLocked: this.props.dieLocked
-        }
-    }
-
-    static getDerivedStateFromProps(props, state) {
-        return { dieFace: props.dieFace, dieValue: props.dieValue, dieLocked: props.dieLocked };
-    };
-
-    render () {
-        console.log('DIE.js in render BOOLEAN');
-        console.log(this.state.dieLocked);
-
-        return (
-            <FontAwesomeIcon onClick={this.props.onLockDie} className={`die ${this.state.dieLocked ? "red-border" : ""}`} icon={this.state.dieFace} size="4x" />
-        );
-    }
+function Die(props) {
+    return (
+        <FontAwesomeIcon onClick={props.onLockDie} className={`die ${props.dieLocked ? "red-border" : ""}`} icon={props.dieFace} size="4x" />
+    )
 }
 
 export default Die;
